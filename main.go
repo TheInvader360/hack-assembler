669d808efbd896f012807b2fea1caf3f07f77b14
Darren <theinvader360@gmail.com>
2021-01-26 22:42:51 +0000

    write output file

--- main.go
+++ main.go
@@ -4,17 +4,34 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) != 2 {
 		fmt.Println("Missing file parameter")
 		return
 	}
-	data, err := ioutil.ReadFile(os.Args[1])
+	inputFilename := os.Args[1]
+
+	if !strings.HasSuffix(inputFilename, ".asm") {
+		fmt.Println("Expected an asm file (*.asm)")
+		os.Exit(1)
+	}
+
+	data, err := ioutil.ReadFile(inputFilename)
 	if err != nil {
-		fmt.Println("Can't read file:", os.Args[1])
+		fmt.Println("Can't read file:", inputFilename)
 		panic(err)
 	}
 	fmt.Println(string(data))
+
+	outputFilename := strings.Replace(inputFilename, ".asm", ".hack", 1)
+
+	output := []byte("lines\nof\ncode\n")
+	err = ioutil.WriteFile(outputFilename, output, 0777)
+	if err != nil {
+		fmt.Println("Can't write file:", outputFilename)
+		panic(err)
+	}
 }
