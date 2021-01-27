867c613d4cb41b8a7e87c62a23f0447f177e08db
Darren <theinvader360@gmail.com>
2021-01-27 02:25:22 +0000

    encode c-commands (unfinished)

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -1,8 +1,10 @@
 package encoder
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Encoder struct {
@@ -22,17 +24,71 @@ func (e *Encoder) EncodeAddressCommand(command string) string {
 }
 
 func (e *Encoder) EncodeComputeCommand(command string) string {
-	return "C-Command" //TODO
+	// Symbolic C-Command Syntax: dest=comp;jump
+	// Binary C-Command Syntax: 111accccccdddjjj
+
+	parts := strings.Split(command, ";")
+	destAndComp := parts[0]
+	jump := ""
+	if len(parts) > 1 {
+		jump = parts[1]
+	}
+
+	parts = strings.Split(destAndComp, "=")
+	dest := parts[0]
+	comp := parts[1]
+
+	comp, _ = lookupComp(comp) // acccccc - TODO error handling
+	dest, _ = lookupDest(dest) // ddd     - TODO error handling
+	jump, _ = lookupJump(jump) // jjj     - TODO error handling
+
+	return fmt.Sprintf("111%s%s%s", comp, dest, jump)
 }
 
-func (e *Encoder) EncodeComp(component string) (string, error) {
-	return "TODO", nil
+func lookupComp(comp string) (string, error) {
+	return "xxxxxxx", nil //TODO
 }
 
-func (e *Encoder) EncodeDest(component string) (string, error) {
-	return "TODO", nil
+func lookupDest(dest string) (string, error) {
+	switch dest {
+	case "":
+		return "000", nil // value not stored
+	case "M":
+		return "001", nil // RAM[A]
+	case "D":
+		return "010", nil // D Register
+	case "MD":
+		return "011", nil // RAM[A] and D Register
+	case "A":
+		return "100", nil // A Register
+	case "AM":
+		return "101", nil // A Register and RAM[A]
+	case "AD":
+		return "110", nil // A Register and D Register
+	case "AMD":
+		return "111", nil // A Register and RAM[A] and D Register
+	}
+	return "", errors.New("Invalid Dest: " + dest)
 }
 
-func (e *Encoder) EncodeJump(component string) (string, error) {
-	return "TODO", nil
+func lookupJump(jump string) (string, error) {
+	switch jump {
+	case "":
+		return "000", nil // no jump
+	case "JGT":
+		return "001", nil // if out > 0 jump
+	case "JEQ":
+		return "010", nil // if out = 0 jump
+	case "JGE":
+		return "011", nil // if out >= 0 jump
+	case "JLT":
+		return "100", nil // if out < 0 jump
+	case "JNE":
+		return "101", nil // if out != 0 jump
+	case "JLE":
+		return "110", nil // if out < 0 jump
+	case "JMP":
+		return "111", nil // unconditional jump
+	}
+	return "", errors.New("Invalid Jump: " + jump)
 }
